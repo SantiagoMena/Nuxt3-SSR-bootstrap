6f4a4322a503c346009b7f78f976f5ccb86b1046
Seriousfighter <schneideralan130892@gmail.com>
2025-02-12 01:58:44 -0300

    nuxt and tailwind

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-red-500">index</h1>
+    <img src="../assets/images/gallery1.png" alt="">
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
